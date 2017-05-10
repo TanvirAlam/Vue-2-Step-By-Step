c1155838f9702a7c8ec2a003d670a68840bb119a
Tanvir Alam <ta@casalogic.dk>
2017-05-10 16:08:28 +0200

    Change the main title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Vue2.js
                 </div>
 
                 <div class="links">
