dc9dc24bf40e1b561c24cb6f43f7c828972d2e60
Tanvir Alam <ta@casalogic.dk>
2017-05-12 11:42:53 +0200

    Setting up vue structure with db included

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -12,9 +12,11 @@ require('./bootstrap');
  * the body of the page. From here, you may begin adding components to
  * the application, or feel free to tweak this setup for your needs.
  */
+var Vue = require('vue');
 
-Vue.component('example', require('./components/Example.vue'));
+Vue.component('alert', require('./components/Alert.vue'));
 
-const app = new Vue({
-    el: '#app'
+new Vue({
+    el: '#app',
+    components: { Alert }
 });
